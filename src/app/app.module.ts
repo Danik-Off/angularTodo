a6f5a388d52ff1fde99ab758010aa60be88ab404
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-10-02 23:12:07 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
+import { AComponent } from './a/a.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    AComponent
   ],
   imports: [
     BrowserModule,
