b6d8a942b8e257466dc52eb9b45401b1297e20f8
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-10-04 16:38:59 +0300

    Update task.component.ts

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,17 +1,43 @@
-import { Component,Input } from '@angular/core';
+import { Component,ElementRef,EventEmitter,Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-task',
   template: `
      <li>
       <input type="checkbox" [checked]="checked"/>
-      <label>{{labelText}}</label>
-      <button>&times;</button>
+      <label #label (dblclick)="onEdit()"
+       [hidden]="isEdited">
+       {{labelText}}</label>
+      <input  (keydown.enter)="onEditEnd()"
+      (blur)="onEditEnd()"
+      [hidden]="!isEdited" type="text" #inputText autofocus/>
+      <button (click)="onDelete()">&times;</button>
     </li>
   `,
   styleUrls: ['./task.component.scss']
 })
 export class TaskComponent {
+isEdited:boolean = false;
+
 @Input("text") labelText:string ='';
 @Input() checked!:boolean;
+@Output() edit = new EventEmitter<string>();
+@Output() delete = new EventEmitter();
+@ViewChild('label')label!: ElementRef;
+@ViewChild('inputText')inputText!: ElementRef;
+
+onDelete(): void{
+
+  this.delete.emit();
+}
+onEdit(): void{
+  this.isEdited = true;
+  this.inputText.nativeElement.value =  this.label.nativeElement.innerText;
+  this.inputText.nativeElement.focus();
+}
+onEditEnd(): void{
+  this.label.nativeElement.innerText =  this.inputText.nativeElement.value ;
+  this.edit.emit(this.inputText.nativeElement.value);
+  this.isEdited = false;
+}
 }
