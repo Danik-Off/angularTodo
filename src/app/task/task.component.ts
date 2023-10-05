7bd746835f6e62048eff4803bdeff16a9e2fe535
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-10-05 16:07:11 +0300

    Update task.component.ts

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,43 +1,63 @@
-import { Component,ElementRef,EventEmitter,Input, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-task',
   template: `
-     <li>
-      <input type="checkbox" [checked]="checked"/>
-      <label #label (dblclick)="onEdit()"
-       [hidden]="isEdited">
-       {{labelText}}</label>
-      <input  (keydown.enter)="onEditEnd()"
-      (blur)="onEditEnd()"
-      [hidden]="!isEdited" type="text" #inputText autofocus/>
+    <li>
+      <input type="checkbox" [checked]="checked" />
+      <label #label (dblclick)="onEdit()" [hidden]="isEdited">
+        {{ editedText}}
+      </label>
+      <input
+        (keydown.enter)="onEditEnd()"
+        (blur)="onEditEnd()"
+        [hidden]="!isEdited"
+        type="text"
+        #inputText
+        [(ngModel)]="editedText"
+
+      />
       <button (click)="onDelete()">&times;</button>
     </li>
   `,
-  styleUrls: ['./task.component.scss']
+  styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent {
-isEdited:boolean = false;
+  editedText: string = ''; // Отдельное поле для редактирования текста
 
-@Input("text") labelText:string ='';
-@Input() checked!:boolean;
-@Output() edit = new EventEmitter<string>();
-@Output() delete = new EventEmitter();
-@ViewChild('label')label!: ElementRef;
-@ViewChild('inputText')inputText!: ElementRef;
+  @Input('text') labelText: string = '';
+  @Input() checked!: boolean;
 
-onDelete(): void{
+  @Output() edit = new EventEmitter<string>();
+  @Output() delete = new EventEmitter();
 
-  this.delete.emit();
-}
-onEdit(): void{
-  this.isEdited = true;
-  this.inputText.nativeElement.value =  this.label.nativeElement.innerText;
-  this.inputText.nativeElement.focus();
-}
-onEditEnd(): void{
-  this.label.nativeElement.innerText =  this.inputText.nativeElement.value ;
-  this.edit.emit(this.inputText.nativeElement.value);
-  this.isEdited = false;
-}
+  @ViewChild('inputText') inputText!: ElementRef;
+
+  isEdited: boolean = false;
+
+
+  ngOnInit(){
+    this.editedText = this.labelText;
+  }
+  onDelete(): void {
+    this.delete.emit();
+  }
+  onEdit(): void {
+    this.isEdited = true;
+    setTimeout(()=>{
+      this.inputText.nativeElement.focus();
+    },0);
+  }
+  onEditEnd(): void {
+
+    this.edit.emit(this.editedText);
+    this.isEdited = false;
+  }
 }
