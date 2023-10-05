1b078a84558e4c2018e446060a47ec95e29b5bd5
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-10-05 16:07:09 +0300

    Update footer.component.ts

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,13 +4,13 @@ import { Component, EventEmitter, Output } from '@angular/core';
   selector: 'app-footer',
   template: `
     <footer>
-      <input type="radio" id="radio1" name="radioGroup"  (change)="filter("all")"/>
+      <input type="radio" id="radio1" name="radioGroup"  (change)="onChange('all')" checked/>
       <label class="custom-radio" for="radio1">All</label>
 
-      <input type="radio" id="radio2" name="radioGroup"  (change)="selectedColor = 'red'"/>
+      <input type="radio" id="radio2" name="radioGroup"  (change)="onChange('active')"/>
       <label class="custom-radio" for="radio2">Active</label>
 
-      <input type="radio" id="radio3" name="radioGroup"  (change)="selectedColor = 'red'"/>
+      <input type="radio" id="radio3" name="radioGroup"  (change)="onChange('done')"/>
       <label class="custom-radio" for="radio3">Completed</label>
     </footer>
   `,
