39dda14b42cd237e61ca35f5921be576b63d4418
Danik-Off <52536804+Danik-Off@users.noreply.github.com>
2023-10-06 12:42:17 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,66 +5,78 @@ import { Task } from './item';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-
 })
 export class AppComponent {
-
-
-  title:string = 'ToDo';
+  title: string = 'ToDo Angular';
 
   filter: 'all' | 'active' | 'done' = 'all';
 
   allItems: Task[] = [];
 
-  get items():Task[]{
+  allSelected: boolean = false;
+
+  get items(): Task[] {
     if (this.filter === 'all') {
       return this.allItems;
     }
-    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+    return this.allItems.filter((item) =>
+      this.filter === 'done' ? item.done : !item.done
+    );
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.load();
   }
+  ngDoCheck() {
+    if (this.allItems.length > 0) {
+      const doneCount = this.allItems.reduce((acc, item) => {
+        if (item.done) acc++;
+        return acc;
+      }, 0);
 
-  setFilter(newFilter:'all' | 'active' | 'done'):void
-  {
-    this.filter = newFilter
+      this.allSelected = doneCount === this.allItems.length;
+    }
   }
-
-  addItem(text:string){
-    this.items.unshift(
-      {done:false,text},
-    )
+  chooseAll(choosenAll: boolean) {
+    console.log("e")
+    this.allItems.map((item) => (item.done = choosenAll));
     this.save();
   }
-  editTextItem(id:number,text:string){
+
+  setFilter(newFilter: 'all' | 'active' | 'done'): void {
+    this.filter = newFilter;
+  }
+
+  addItem(text: string) {
+    this.allItems.unshift({ done: false, text });
     this.save();
   }
-  editStatusItem(id:number){
+  editTextItem(id: number, text: string) {
+    this.allItems[id].text = text;
+    this.save();
+  }
+  editStatusItem(id: number, done: boolean) {
+    console.log(done);
+    this.allItems[id].done = done;
     this.save();
   }
   deleteItem(id: number) {
     if (id >= 0 && id < this.items.length) {
-      this.items.splice(id, 1);
+      this.allItems.splice(id, 1);
       this.save();
     }
   }
-  save(){
-    localStorage.setItem(
-      "todoItems",JSON.stringify(this.allItems)
-    );
+  save() {
+    localStorage.setItem('todoItems', JSON.stringify(this.allItems));
   }
-  load(){
-    const localStorageData = localStorage.getItem( "todoItems");
+  load() {
+    const localStorageData = localStorage.getItem('todoItems');
     if (localStorageData) {
       try {
         this.allItems = JSON.parse(localStorageData) as Task[];
       } catch (error) {
-        console.error("Ошибка:", error);
+        console.error('Ошибка:', error);
       }
     }
-
   }
 }
-
